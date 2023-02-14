47176db80caecd565c4f2f8b08988e8e87e5afd6
lummendoza <luismiguelmm95@gmail.com>
2023-02-14 01:07:34 +0000

    Page Ready

---
+++ src/js/component/PageBottom.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+//create your first component
+const PageBottom = () => {
+	return (
+        <div className="fixed-bottom bg-secondary d-flex justify-content-center">
+            <p>copyright</p>
+        </div>
+    );
+};
+
+export default PageBottom;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import NavBar from "../../js/component/NavBar.jsx";
 import PageTop from "../../js/component/PageTop.jsx";
+import PageMid from "../../js/component/PageMid.jsx";
+import PageBottom from "../../js/component/PageBottom.jsx";
 
 
 //create your first component
@@ -11,10 +13,11 @@ const Home = () => {
 	return (
 		<div className="page">
 			<NavBar/>
-			<div className="container d-flex justify-content-center p-10">
+			<div className="container d-flex flex-column justify-content-center p-10">
 				<PageTop/>
-
+				<PageMid/>
 			</div>
+			<PageBottom/>
 
 			
 			
